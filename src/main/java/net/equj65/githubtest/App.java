4b30b5ecca1fbd8bed2992f905444df0853911dd
Ryozo Watanabe <clearstor@gmail.com>
2015-07-15 21:18:34 +0900

    依存性テスト用のクラス追加

---
+++ src/main/java/net/equj65/githubtest/App.java
@@ -0,0 +1,14 @@
+package net.equj65.githubtest;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * Hello world!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(StringUtils.trim("  test  "));
+    }
+}
